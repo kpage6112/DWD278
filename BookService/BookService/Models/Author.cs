a82fb869271d66b427dec27f9e20c576fb258101
KRISTEN McIntosh <KPAGE6112@my.sctd.edu>
2018-04-23 22:35:24 -0400

    ini

---
+++ BookService/BookService/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
